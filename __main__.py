52c1e02af7f04f1ad939239e159f0fcf004e86ce
Lucca Machado <lucca.machado@uem.com.br>
2024-10-12 17:30:05 -0300

    Added mqtt paho client

--- __main__.py
+++ __main__.py
@@ -1,22 +1,44 @@
 import cantools
 import can
+import sys
 
 import paho.mqtt.client as mqtt
 
 
 
+def onMessageMQTT(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload.decode()}")
 
 
 def systemCAN_ReceiveMessage(msg):
     pass
 
 def main():
+    # Initialize MQTT Client
+    mqttClient = mqtt.Client()
+    mqttClient.on_message = onMessageMQTT
+
+    if mqttClient.connect("localhost", 1883, 60) != 0:
+        print("Could not connect to MQTT Broker!")
+        sys.exit(-1)
+    
+    # Initialize CAN-BUS
     systemCAN_Bus = can.Bus('can0', bustype='socketcan', bitrate=250000)
     systemCAN_Printer = can.Printer()
 
     systemCAN_Bus_Notifier = can.Notifier(systemCAN_Bus, [systemCAN_Printer, systemCAN_ReceiveMessage])
 
-    input(f"Press any key to exit...")
+    
+    mqttClient.subscribe("test/ping")
+
+    try:
+        print("Press CTRL+C to exit...")
+        mqttClient.loop_forever()
+    except:
+        systemCAN_Bus_Notifier.stop()
+        print("Disconnecting from broker")
+
+    mqttClient.disconnect()
 
     
 
